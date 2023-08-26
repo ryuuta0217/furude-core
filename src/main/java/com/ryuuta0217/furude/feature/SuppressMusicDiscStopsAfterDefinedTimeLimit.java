748666312f1c074f6c7c53163af69c9930bc117a
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2023-08-26 15:56:42 +0900

    バグ修正: レコードの強制停止抑制の代替策
    e4871ef429b7b959b0d6a6c1084f79644062a2e9

--- src/main/java/com/ryuuta0217/furude/feature/SuppressMusicDiscStopsAfterDefinedTimeLimit.java
+++ src/main/java/com/ryuuta0217/furude/feature/SuppressMusicDiscStopsAfterDefinedTimeLimit.java
@@ -1,6 +1,12 @@
 package com.ryuuta0217.furude.feature;
 
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.JukeboxBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.unknown.core.util.MinecraftAdapter;
 import org.bukkit.GameEvent;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.GenericGameEvent;
@@ -12,8 +18,17 @@ import org.bukkit.event.world.GenericGameEvent;
 public class SuppressMusicDiscStopsAfterDefinedTimeLimit implements Listener {
     @EventHandler
     public void onGenericGameEvent(GenericGameEvent event) {
-        if (event.getEvent() == GameEvent.JUKEBOX_STOP_PLAY) {
-            event.setCancelled(true);
+        if (event.getEvent() == GameEvent.JUKEBOX_PLAY) {
+            Block block = event.getLocation().getBlock();
+            if (block.getType() == Material.JUKEBOX) {
+                BlockState state = MinecraftAdapter.blockState(block);
+                if (state.hasBlockEntity()) {
+                    BlockEntity entity = MinecraftAdapter.level(block.getWorld()).getBlockEntity(MinecraftAdapter.blockPos(event.getLocation()));
+                    if (entity instanceof JukeboxBlockEntity jukebox) {
+                        jukebox.isPlaying = false;
+                    }
+                }
+            }
         }
     }
 }
